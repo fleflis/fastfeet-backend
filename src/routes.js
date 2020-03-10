3a7b1e3426c5393a8b84291eed6d528d5fbc115b
Christian Sanches <christian.sanches@gogodigital.com.br>
2020-03-10 15:18:05 -0300

    route recipients

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientsController from './app/controllers/RecipientsController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/recipients', RecipientsController.store);
 
 export default routes;
