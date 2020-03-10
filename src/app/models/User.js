237c671a838c56700cfd8d19f050002000c8a1c8
Christian Sanches <christian@endless-point.org>
2020-03-09 21:17:58 -0300

    hash de senha automático

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+
 import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
@@ -6,6 +8,7 @@ class User extends Model {
 			{
 				name: Sequelize.STRING,
 				email: Sequelize.STRING,
+				password: Sequelize.VIRTUAL,
 				password_hash: Sequelize.STRING,
 				admin: Sequelize.BOOLEAN,
 			},
@@ -13,6 +16,14 @@ class User extends Model {
 				sequelize,
 			}
 		);
+
+		this.addHook('beforeSave', async user => {
+			if (user.password) {
+				user.password_hash = await bcrypt.hashSync(user.password, 8);
+			}
+		});
+
+		return this;
 	}
 }
 
