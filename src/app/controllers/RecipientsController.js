eae9d3c211eb3ff2592cba9a81b125d36ded05a3
Christian Sanches <christian@endless-point.org>
2020-03-16 20:36:54 -0300

    Validação RecipientsController store

--- src/app/controllers/RecipientsController.js
+++ src/app/controllers/RecipientsController.js
@@ -1,18 +1,22 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientsController {
 	async store(req, res) {
-		// @TODO: Validação dos dados de entrada.
+		const recipientSchema = Yup.object.shape({
+			name: Yup.string().required(),
+			street: Yup.string().required(),
+			number: Yup.string().required(),
+			complement: Yup.string(),
+			state: Yup.string().required(),
+			city: Yup.string().required(),
+			zip_code: Yup.number().required(),
+		});
 
-		// const {
-		// 	name,
-		// 	street,
-		// 	number,
-		// 	complement,
-		// 	state,
-		// 	city,
-		// 	zip_code,
-		// } = req.body;
+		if (!(await recipientSchema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Validation Field' });
+		}
 
 		const recipient = await Recipient.create(req.body);
 		return res.status(201).json(recipient);
